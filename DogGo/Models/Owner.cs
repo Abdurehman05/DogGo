c3ec01001c338b161b64dcb8656ed011571d9743
Abdu <abdukr5@gmail.com.com>
2020-12-10 23:33:17 -0600

    Owner properties and repos created

---
+++ DogGo/Models/Owner.cs
@@ -0,0 +1,15 @@
+﻿using DogGo.Models;
+
+namespace DogGo.Repositories
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public int NeighborhoodId { get; set; }
+        public Neighborhood Neighborhood { get; set; }
+    }
+}
