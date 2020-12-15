253758ab89b2b7d3e92e4d9b1811b413a34967e7
Abdu <abdukr5@gmail.com.com>
2020-12-14 23:35:01 -0600

    Walks repository with methods needed for walker profile created

---
+++ DogGo/Repositories/IWalksRepository.cs
@@ -0,0 +1,12 @@
+﻿using DogGo.Models;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public interface IWalkRepository
+    {
+        List<Walks> GetAllWalks();
+        Walks GetWalkById(int id);
+        List<Walks> GetWalksByWalkerId(int walkerId);
+    }
+}
